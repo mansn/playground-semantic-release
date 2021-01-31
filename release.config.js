1541d09c53c955fa67b38e3894c437907a2ff083
mansn <4518977+mansn@users.noreply.github.com>
2021-01-31 23:27:54 +0100

    fix: run workflow and semantic-release on feature and bugfix branches

--- release.config.js
+++ release.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  branches: [
+    "+([0-9])?(.{+([0-9]),x}).x",
+    "main",
+    { name: "feature", prerelease: true },
+    { name: "bugfix", prerelease: true }
+  ],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
